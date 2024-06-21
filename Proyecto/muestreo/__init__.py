2b2a54b7b73a5ffa8dd0a3d4ba3ac8d1e900126d
Ayelen <144490648+ayescafati@users.noreply.github.com>
2024-06-21 03:22:38 -0300

    Delete Proyecto/muestreo directory

--- Proyecto/muestreo/__init__.py
+++
@@ -1,3 +0,0 @@
-from muestreo.bootstraps import generar_bootstraps # noqa
-from muestreo.validacion_cruzada import generar_k_subconjuntos # noqa
-from muestreo.split import dividir_entrenamiento_y_pruebas # noqa
