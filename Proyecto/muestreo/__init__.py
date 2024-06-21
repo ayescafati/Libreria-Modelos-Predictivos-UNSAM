299e7fa3a37f9c8967decd543f6bf488dacd7353
Ayelen <144490648+ayescafati@users.noreply.github.com>
2024-06-21 03:23:22 -0300

    Add files via upload

---
+++ Proyecto/muestreo/__init__.py
@@ -0,0 +1,3 @@
+from muestreo.bootstraps import generar_bootstraps # noqa
+from muestreo.validacion_cruzada import generar_k_subconjuntos # noqa
+from muestreo.split import dividir_entrenamiento_y_pruebas # noqa
