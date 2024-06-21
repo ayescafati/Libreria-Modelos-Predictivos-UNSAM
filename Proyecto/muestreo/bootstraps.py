f22cf71a5535bb0b9a33ba5f62ed1aaf11dae121
Ayelen <144490648+ayescafati@users.noreply.github.com>
2024-06-21 03:21:00 -0300

    Add files via upload

---
+++ Proyecto/muestreo/bootstraps.py
@@ -0,0 +1,11 @@
+from numpy.random import randint
+from typing import List
+import pandas as pd 
+
+def generar_bootstraps(df: pd.DataFrame, numero_muestras: int, semilla: int = randint(0, 10000)) -> List[pd.DataFrame]:
+    bootstraps = []
+    for i in range(numero_muestras):
+        muestra = df.sample(frac=1, replace = True, random_state=semilla+i) # en Matemática y/o Estadística computacional entendemos por "semilla" a aquel nuúero (o vector) utilizado para inicializar un generador de números pseudoaleatorios
+        bootstraps.append(muestra)
+
+    return bootstraps
--- Proyecto/muestreo/validacion_cruzada.py
+++ Proyecto/muestreo/validacion_cruzada.py
@@ -1,27 +1,34 @@
 import pandas as pd
 from numpy.random import randint
+from typing import List, Tuple
 
-
-def subconjuntos_aleatorios(df, k, agregar_restantes, semilla): 
-    dimension_subconjunto = len(df) // k # k es el numero de subconjuntos o particiones en los que se divide el conjunto de datos original para el proceso de validacion cruzada
+def subconjuntos_aleatorios(df: pd.DataFrame, k: int, agregar_restantes: bool, semilla: int) -> List[pd.DataFrame]:
+    dimension_subconjunto = len(df) // k # k es el numero de subconjuntos o particiones en los que se divide el conjunto de datos original para el proceso de validacion cruzada.
 
     subconjuntos = []
     for i in range(k):
-        muestra = df.sample(n=dimension_subconjunto, random_state=semilla)
+        muestra = df.sample(n = dimension_subconjunto, random_state=semilla)
         df = df.drop(muestra.index, errors = 'ignorar')
         subconjuntos.append(muestra)
 
-    if agregar_restantes: # agregamos los elementos restantes a los subconjuntos
+    if agregar_restantes: # Agregamos los elementos restantes a los subconjuntos.
         for i in range(len(df)):
             subconjuntos[i] = pd.concat([subconjuntos[i], df.iloc[i:i+1]])
 
     return subconjuntos
 
 
-def generar_k_subconjuntos(df, k, tipo_muestreo = 'estratificado', agregar_restantes = True, semilla = randint(10000)):
+def estratificacion_de_los_subconjuntos(df: pd.DataFrame, k: int, agregar_restantes: bool, semilla: int) -> List[pd.DataFrame]: # La estratificación es aquella técnica de muestreo que se utiliza para garantizar que las proporciones de las clases en los datos de entrenamiento y prueba sean lo más similares posibles.
+    grupos = df.groupby('clase')
+    subconjutons_por_grupo = [subconjuntos_aleatorios(grupo, k, agregar_restantes, semilla) for valor_clase, grupo in grupos]
+    subconjuntos = [pd.concat(subconjutons_por_grupo[x][y] for x in range(len(subconjutons_por_grupo))) for y in range(k)]
+    return subconjuntos
+
+
+def generar_k_subconjuntos(df: pd.DataFrame, k: int, tipo_muestreo: str = 'estratificado', agregar_restantes: bool = True, semilla: int = randint(0, 10000)) -> List[pd.DataFrame]:
     if tipo_muestreo == 'aleatorio':
         return subconjuntos_aleatorios(df, k, agregar_restantes, semilla)
     elif tipo_muestreo == 'estratificado':
         return estratificacion_de_los_subconjuntos(df, k, agregar_restantes, semilla)
     else:
-        raise Exception("El parametro de muestreo debe ser estratificado o aleatorio.")
+        raise Exception("El parametro de muestreo debe ser uno de los siguientes: [estratificado, aleatorio]")
